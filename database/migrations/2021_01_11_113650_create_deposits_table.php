b509812fb7f7c2404ecab4a6644a36a9be2529a7
Anatoliy Vetrenko <anatoliy.vetrenko@gmail.com>
2021-01-11 14:27:19 +0200

    add models folder + migration

---
+++ database/migrations/2021_01_11_113650_create_deposits_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDepositsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    public function up()
+    {
+        Schema::create('deposits', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->bigInteger('wallet_id')->unsigned()->index();
+            $table->double('invested');
+            $table->double('percent');
+            $table->smallInteger('active');
+            $table->smallInteger('duration');
+            $table->smallInteger('accrue_times');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('wallet_id')->references('id')->on('wallets');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deposits');
+    }
+}
---
+++ database/migrations/2021_01_11_113704_create_transactions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('type');
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->bigInteger('wallet_id')->unsigned()->index();
+            $table->bigInteger('deposit_id')->unsigned()->index();
+            $table->double('amount');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('wallet_id')->references('id')->on('wallets');
+            $table->foreign('deposit_id')->references('id')->on('deposits');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
